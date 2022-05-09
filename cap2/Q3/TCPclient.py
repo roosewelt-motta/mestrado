25f54dae5904858ab854c8377dee4cdf20e0ba5d
Roosewelt Motta <roosewelt.motta@gmail.com>
2022-05-09 20:36:06 -0300

    Cap 3

---
+++ cap2/Q3/TCPclient.py
@@ -0,0 +1,11 @@
+from socket import *
+serverName = 'localhost'
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+while True:
+    sentence = input('Input lowercase sentence: ')
+    clientSocket.send(sentence.encode())
+    modifiedSentence = clientSocket.recv(2048)
+    print('From Server: ', modifiedSentence.decode())
+clientSocket.close()
---
+++ cap2/Q3/TCPserver.py
@@ -0,0 +1,31 @@
+from socket import *
+import _thread
+
+host = ''
+serverPort = 12000
+
+def conexao(con, cli):
+    print('Client: ', cli)
+
+    while True:
+        sentence = con.recv(1024).decode()
+        if not sentence:
+            break
+        capSen = sentence.upper()
+        con.send(capSen.encode())
+        print(cli,' -> ',capSen) 
+
+    print('Close ', cli)
+    con.close()
+    _thread.exit()
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind((host, serverPort))
+serverSocket.listen(1)
+
+while True:
+    print('The server is ready to receive')
+    con,cli =  serverSocket.accept()
+    _thread.start_new_thread(conexao,(con,cli))
+
+serverSocket.close()
